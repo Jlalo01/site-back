9e0a56e7422b6a3a1d0b68960bf71a5117aeb4af
test <jl@Joeys-MacBook-Pro-3.local>
2023-09-26 14:45:46 -0500

    added get photos and ord-photos

--- app.js
+++ app.js
@@ -57,15 +57,37 @@ app.get("/categories", async (req, res) => {
     res.json(data);
 });
 
+//Send the data for a specific category
+app.get("/category/:theCat", async (req, res) => {
+    const data = await category.find({name:req.params.theCat});
+    res.json(data);
+});
+
 //Send all the videos that fit a specific category set in the parameters
 app.get("/video/cat/:theCat", async (req, res) => {
     const data = await video.find({category:req.params.theCat});
     res.json(data);
 });
 
+//Send all the videos that fit a specific category set in parameters in order, newest at 0 index
 app.get("/video/ord-cat/:theCat", async (req, res) => {
     const data = await video.find({category:req.params.theCat});
     data.sort((a, b) => {
+        return b.createdAt - a.createdAt;
+    });
+    res.json(data);
+});
+
+//Send all photos
+app.get("/photos", async (req, res) =>{
+    const data = await photo.find({});
+    res.json(data);
+});
+
+//Send all photos in order, newest at 0 index
+app.get("photos-ord", async (req, res) => {
+    const data = await photo.find({});
+    data.sort((a, b) =>{
         return b.createdAt - a.createdAt;
     });
     res.json(data);
@@ -77,13 +99,34 @@ app.get("/slides", async (req, res)=> {
     res.json(data);
 });
 
+//Send the Programming projects 
 app.get("/prog", async (req, res) =>{
     const data = await prog.find({}).exec();
     res.json(data)
 });
 
+//Create a new Video object 
+app.post("/up-video", async (req, res) => {
+    const data = req.body;
+    res.send("hello");
+});
 
+//Add or create to a new category
+app.post("/up-category", async (req, res) => {
+    const data = req.body;
+    const c = await category.find({name:data[1].name});
+    try{
+        if (c.length === 0){await category.create(data[1]);}
+        else {await category.findOneAndReplace(data[0], data[1]);}
+    } catch(err){
+        console.log(err);
+        res.send(false);
+    } finally{
+        res.send(true);
+    }
+});
 
+//Create a new Programming entry
 app.post("/up-prog", async (req, res) => {
     const data = req.body;
     const u = await user.findOne({user:"jlalo"});
